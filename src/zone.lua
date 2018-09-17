16677372f7016956675e61a2eafbe794fc2da10e
atman <atman>
2018-09-17 16:03:39 +0000

    Collision algorithms hurt my brain
    This is non-working but I'm running out of steam for the day

--- src/zone.lua
+++ src/zone.lua
@@ -81,12 +81,12 @@ local Zoneherd = meta {}
 
 
 
-local function newZone(tr, tc, br, bc, z, debug_mark)
+local function newZone(tc, tr, bc, br, z, debug_mark)
    local zone = meta(Zone)
-   zone.tr = tr
    zone.tc = tc
-   zone.br = br
+   zone.tr = tr
    zone.bc = bc
+   zone.br = br
    zone.z = z
    zone.debug_mark = debug_mark
    return zone
@@ -96,12 +96,60 @@ end
 
 
 
-function Zoneherd:newZone(zoneherd, name, tr, tc, br, bc, z, debug_mark)
-   zoneherd[name] = newZone(tr, tc, br, bc, z, debug_mark)
+
+
+
+
+
+
+
+
+
+local function _inside(col, row, zone)
+   return (col >= zone.tc)
+      and (col <= zone.bc)
+      and (row >= zone.tr)
+      and (row <= zone.br)
+end
+
+function _collide(z_a, z_b)
+   local z_a_corners = { {z_a.tc, z_a.tr},
+                         {z_a.tc, z_a.br},
+                         {z_a.bc, z_a.tr},
+                         {z_a.bc, z_a.br} }
+   local hits = {}
+   for i, corner in ipairs(z_a_corners) do
+      hits[i] = _inside(corner[1], corner[2], z_b)
+   end
+   return hits
+end
+
+
+
+
+
+
+
+function Zoneherd:newZone(zoneherd, name, tc, tr, bc, br, z, debug_mark)
+   zoneherd[name] = newZone(tc, tr, bc, br, z, debug_mark)
    -- this doesn't account for Z axis but for now:
    zoneherd[#zoneherd + 1] = zoneherd[name]
+   -- todo: make a Zoneherd:add(zone, name) that handles z-ordering
+   -- and auto-adjusts proportionally.
    return zoneherd
 end
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -116,11 +164,14 @@ local function new(modeS, writer)
    local zoneherd = meta(Zoneherd)
    zoneherd.write = writer
    -- make Zones
-   -- (top_row, top_col, bottom_row, bottom_col, z, debug-mark)
-   zoneherd.command = newZone(2, modeS.l_margin, 80, 2, 0, "|")
+   -- (top_col, top_row, bottom_col, bottom_row, z, debug-mark)
+   zoneherd.command = newZone(modeS.l_margin,     modeS:replLine(),
+                              modeS.max_col - 20, 2,
+                              0, "|")
    zoneherd[1] = zoneherd.command
-   zoneherd.results = newZone(modeS:replLine() + 1,
-                              modeS.l_margin, 80, 30, 0, "~")
+   zoneherd.results = newZone(modeS.l_margin,     modeS:replLine(),
+                              modeS.max_col - 20, modeS.max_row,
+                              0, "~")
    zoneherd[2] = zoneherd.results
    return zoneherd
 end
