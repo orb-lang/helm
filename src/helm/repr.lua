87a9ec1e1d8767bb34797e535f56a055beeef65a
Daniel Slomovits <daniels220@gmail.com>
2019-12-13 13:32:58 -0500

    Minor tweaks to repr.ts and repr.lineGen

--- src/helm/repr.lua
+++ src/helm/repr.lua
@@ -432,9 +432,10 @@ end
 
 
 
-function repr.ts(val, disp_width)
+
+function repr.ts(val, disp_width, color)
    local phrase = {}
-   for line in repr.lineGen(val, disp_width, C.no_color) do
+   for line in repr.lineGen(val, disp_width, color or C.no_color) do
       phrase[#phrase + 1] = line
    end
    return concat(phrase, "\n")
@@ -449,12 +450,7 @@ end
 
 
 function repr.ts_color(val, disp_width, color)
-   local phrase = {}
-   color = color or C.color
-   for line in repr.lineGen(val, disp_width, color) do
-      phrase[#phrase + 1] = line
-   end
-   return concat(phrase, "\n")
+   return repr.ts(val, disp_width, color or C.color)
 end
 
 
