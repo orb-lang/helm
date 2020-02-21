46130d6921508729b50c6e9da38332912a3cf78a
Daniel Slomovits <daniels220@gmail.com>
2020-02-21 13:10:57 -0500

    Implement accepting suggestions

--- src/helm/raga/complete.lua
+++ src/helm/raga/complete.lua
@@ -23,7 +23,12 @@ local function _quit(modeS, category, value)
    modeS:shiftMode(modeS.raga_default)
 end
 
-Complete.PASTE = function(modeS, category, value)
+local function _accept(modeS, category, value)
+   modeS.suggest:accept(modeS)
+   _quit(modeS, category, value)
+end
+
+function Complete.PASTE(modeS, category, value)
    _quit(modeS, category, value)
    modeS.modes.PASTE(modeS, category, value)
 end
@@ -37,10 +42,15 @@ local _default_insert = Complete.ASCII
 
 local function _insert(modeS, category, value)
    -- Non-symbol character accepts the completion
+   -- #todo should be consistent with lex.orb definition
    if find(value, "[^a-zA-Z0-9_]") then
-      modeS.suggest:accept()
+      _accept(modeS, category, value)
+      -- Retry with the new raga
+      -- #todo should we defer to modeS:act() here instead?
+      modeS.modes[category](modeS, category, value)
+   else
+      _default_insert(modeS, category, value)
    end
-   _default_insert(modeS, category, value)
 end
 
 Complete.ASCII = _insert
@@ -67,10 +77,18 @@ NAV.UP = NAV.SHIFT_TAB
 NAV.SHIFT_UP = NAV.SHIFT_TAB
 
 NAV.ESC = _quit
-
-function NAV.RETURN(modeS, category, value)
-   modeS.suggest:accept()
+function NAV.RIGHT(modeS, category, value)
    _quit(modeS, category, value)
+   -- #todo dispatch this properly, technically the new raga
+   -- may not have a handler for it
+   modeS.modes.NAV.RIGHT(modeS, category, value)
+end
+
+NAV.RETURN = _accept
+function NAV.LEFT(modeS, category, value)
+   _accept(modeS, category, value)
+   -- #todo dispatch this properly
+   modeS.modes.NAV.LEFT(modeS, category, value)
 end
 
 
