3200e4a296d00eb0d48972c67cc6d05d70db31b0
Daniel Slomovits <daniels220@gmail.com>
2020-11-23 13:55:38 -0500

    Intercept quit command in --new-session mode to enter review
    This is a godawful hack, as documented in a comment, but it does work

--- src/helm/raga/base.lua
+++ src/helm/raga/base.lua
@@ -44,7 +44,26 @@ end
 
 
 RagaBase.CTRL["^Q"] = function(modeS, category, value)
-   modeS:quit()
+   -- #todo it's obviously terrible to have code specific to a particular
+   -- piece of functionality in an abstract class like this.
+   -- To do this right, we probably need a proper raga stack. Then -n could
+   -- push the Review raga onto the bottom of the stack, then Nerf. Quit
+   -- at this point would be the result of the raga stack being empty,
+   -- rather than an explicitly-invoked command, and Ctrl-Q would just pop
+   -- the current raga. Though, a Ctrl-Q from e.g. Search would still want
+   -- to actually quit, so it's not quite that simple...
+   if _Bridge.args.new_session then
+      -- #todo Add the ability to change accepted status of
+      -- the whole session to the review interface
+      modeS.hist.session.accepted = true
+      -- Also, it's horribly hacky to change the "default" raga, but it's
+      -- the only way to make Modal work properly. A proper raga stack
+      -- would *definitely* fix this
+      modeS.raga_default = "review"
+      modeS.shift_to = "review"
+   else
+      modeS:quit()
+   end
 end
 
 
