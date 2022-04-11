0f5af57c3bfc3c1a9c8a3a51cfda0d6abc884919
Daniel Slomovits <daniels220@gmail.com>
2022-04-11 14:52:17 -0400

    Move action lookup to a new callable Keymap module
    Keymap structure and construction process is unchanged

--- src/helm/maestro.lua
+++ src/helm/maestro.lua
@@ -14,8 +14,6 @@
 
 
 
-local input_event = require "anterm:input-event"
-
 local EditAgent      = require "helm:agent/edit"
 local InputEchoAgent = require "helm:agent/input-echo"
 local ModalAgent     = require "helm:agent/modal"
@@ -36,6 +34,7 @@ local table = core.table
 
 
 local cluster = require "cluster:cluster"
+local Keymap = require "helm:keymap"
 
 local Actor = require "actor:actor"
 
@@ -60,47 +59,19 @@ local new, Maestro, Maestro_M = cluster.genus(Actor)
 
 
 
-local gmatch = assert(string.gmatch)
 local clone, insert = assert(table.clone), assert(table.insert)
-local Message = require "actor:message"
 local assert = assert(core.fn.assertfmt)
 local _yield  = assert(core.thread.nest "actor" .yield)
 
 function Maestro.activeKeymap(maestro)
-   local composed_keymap = { bindings = {}, wildcards = {} }
    local keymap_list = maestro.modeS.raga.default_keymaps
    for _, keymap in ipairs(keymap_list) do
-      local bindings = maestro:dispatch { to = keymap.source,
+      keymap.bindings = maestro:dispatch { to = keymap.source,
                                           field  = keymap.name }
-      assert(bindings, "Failed to retrieve bindings for " ..
+      assert(keymap.bindings, "Failed to retrieve bindings for " ..
                keymap.source .. "." .. keymap.name)
-      for key, action in pairs(bindings) do
-         -- #todo assert that this is either a string or Message?
-         if type(action) == "string" then
-            -- See :dispatchEvent()--by leaving out .n, we cause the command
-            -- to be executed with no arguments
-            -- @sam: is this a sensible use of .n?
-            action = { method = action }
-         else
-            action = clone(action)
-         end
-         action.sendto = action.sendto or keymap.source
-         -- Right now modeS ends up mutating these to route stuff to agents
-         -- properly, and Message is read-only. Once we have a proper
-         -- polymorphic dispatchmessage, or just a reasonable workaround,
-         -- we should start converting these
-         -- action = Message(action)
-         local key_evt = input_event.marshal(key)
-         assert(key_evt, "Failed to parse event string: '%s'", key)
-         if key_evt.type == "wildcard" then
-            insert(composed_keymap.wildcards, { pattern = key_evt, action = action })
-         else
-            composed_keymap.bindings[key] = composed_keymap.bindings[key] or {}
-            insert(composed_keymap.bindings[key], action)
-         end
-      end
    end
-   return composed_keymap
+   return Keymap(unpack(keymap_list))
 end
 
 
@@ -182,39 +153,17 @@ end
 
 
 
-local concat = assert(table.concat)
 
-local function is_wildcard_match(wc_evt, evt)
-   if wc_evt.modifiers ~= evt.modifiers then
-      return false
-   end
-   if evt.type == "keypress" then
-      local special = input_event.is_special_key(evt.key)
-      if wc_evt.key == "[CHARACTER]" and not special
-      or wc_evt.key == "[SPECIAL]" and special then
-         return true
-      end
-   end
-   if wc_evt.key == "[MOUSE]" and evt.type == "mouse" then
-      return true
-   end
-   return false
-end
+local concat = assert(table.concat)
 
 local function _dispatchOnly(maestro, event)
    local keymap = maestro:activeKeymap()
-   local event_string = input_event.serialize(event)
-   local handlers = clone(keymap.bindings[event_string] or {})
-   for _, wc_dict in ipairs(keymap.wildcards) do
-      if is_wildcard_match(wc_dict.pattern, event) then
-         insert(handlers, wc_dict.action)
-      end
-   end
+   local handlers = keymap(event)
    local tried = {}
    for _, handler in ipairs(handlers) do
       handler = clone(handler)
       -- #todo make this waaaaay more flexible
-      if handler.n and handler.n > 0 then
+      if handler.n > 0 then
          handler[handler.n] = event
       end
       -- #todo ugh, some way to dump a Message to a representative string?
